97374803e8ea2252a821a1519b120911194d70c3
rajithahari <rajitha081189@gmail.com>
2020-05-18 19:28:30 +0530

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,3 +2,21 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserRegisterFormComponent } from './user-register-form/user-register-form.component';
 import { DisplayUserInfoComponent } from './display-user-info/display-user-info.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UserRegisterFormComponent
+  },
+  {
+    path: 'user/:uid',
+    component: DisplayUserInfoComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const RoutingComponent = [UserRegisterFormComponent, DisplayUserInfoComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register.component';
+import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { UserRegisterFormComponent } from './user-register-form/user-register-form.component';
 import { DisplayUserInfoComponent } from './display-user-info/display-user-info.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, RegisterComponent, UserRegisterFormComponent, DisplayUserInfoComponent ],
+  imports:      [ BrowserModule, FormsModule , AppRoutingModule],
+  declarations: [ AppComponent, RegisterComponent, UserRegisterFormComponent, DisplayUserInfoComponent, RoutingComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
