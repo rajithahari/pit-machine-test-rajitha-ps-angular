9e9691f3b00ce3ddb7f546280ad564574e3d8728
rajithahari <rajitha081189@gmail.com>
2020-05-18 19:11:29 +0530

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<div class="d-flex align-items-center"><button type="button" class="btn btn-primary">Register</button></div>`
+  template: `<button type="button" class="btn btn-primary">Register</button>`
 })
 export class HelloComponent  {
 }
