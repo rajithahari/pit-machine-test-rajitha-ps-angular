92d9455737d923cccdc34bc41c37ccdfc7f90973
rajithahari <rajitha081189@gmail.com>
2020-05-18 21:21:23 +0530

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserRegisterFormComponent } from './user-register-form/user-register-form.component';
 import { DisplayUserInfoComponent } from './display-user-info/display-user-info.component';
+import { RegisterComponent } from './register.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: RegisterComponent
+  },
   {
     path: 'register',
     component: UserRegisterFormComponent
