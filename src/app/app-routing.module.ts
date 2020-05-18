831513bdaa55c5943fe1ef967e899860f3ebe576
rajithahari <rajitha081189@gmail.com>
2020-05-18 19:25:50 +0530

    1 file added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,4 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UserRegisterFormComponent } from './user-register-form/user-register-form.component';
+import { DisplayUserInfoComponent } from './display-user-info/display-user-info.component';
