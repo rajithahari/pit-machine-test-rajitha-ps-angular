96ae1dcea26515ee4772489e57d7980e06545515
rajithahari <rajitha081189@gmail.com>
2020-05-19 17:38:48 +0530

    1 file modified

--- src/app/user-register-form/user-register-form.component.ts
+++ src/app/user-register-form/user-register-form.component.ts
@@ -1,17 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-user-register-form',
+  selector: 'user-register-form',
   templateUrl: './user-register-form.component.html',
-  styleUrls: ['./user-register-form.component.css']
+  styleUrls: ['./user-register-form.component.sass']
 })
 export class UserRegisterFormComponent implements OnInit {
+  registered = false;
+  submitted = false;
+  userForm: FormGroup;
+  guid: string;
 
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-    
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) {
   }
 
+  invalidFirstName() {
+    return (this.submitted && this.userForm.controls.firstName.errors != null);
+  }
+
+  invalidLastName() {
+    return (this.submitted && this.userForm.controls.lastName.errors != null);
+  }
+
+  invalidEmail() {
+    return (this.submitted && this.userForm.controls.email.errors != null);
+  }
+
+
+  ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      firstName: ['', Validators.required, Validators.pattern('^[a-zA-Z]{20}')],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.userForm.invalid === true) {
+      return;
+    } else {
+      const data = this.userForm.value;
+    }
+  }
 }
